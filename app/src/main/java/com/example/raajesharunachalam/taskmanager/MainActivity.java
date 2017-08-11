f4a9417f6da26807dff0c1242898a4f4cf53fa64
vishnujaganathan <30843744+vishnujaganathan@users.noreply.github.com>
2017-08-11 16:52:12 -0500

    added signup page and fixed listeners

--- app/src/main/java/com/example/raajesharunachalam/taskmanager/MainActivity.java
+++ app/src/main/java/com/example/raajesharunachalam/taskmanager/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.raajesharunachalam.taskmanager;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,11 +12,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button login_button = (Button) findViewById(R.id.login);
+        login_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToLoginPage();
+            }
+        });
+
+        Button signup_button = (Button) findViewById(R.id.signup);
+        signup_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToSignUpPage();
+            }
+        });
     }
 
     public void goToLoginPage()
     {
         Intent i = new Intent(this, LoginActivity.class);
+        startActivity(i);
+    }
+
+    public void goToSignUpPage()
+    {
+        Intent i = new Intent(this, SignUpPage.class);
         startActivity(i);
     }
 }
