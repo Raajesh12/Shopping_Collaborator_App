58847ae16a8b31710256ca60264d9640c41cb6f9
Raajesh12 <raajstar3@gmail.com>
2017-08-11 19:06:02 -0500

    TasksActivity

--- app/src/main/java/com/example/raajesharunachalam/taskmanager/TasksActivity.java
+++ app/src/main/java/com/example/raajesharunachalam/taskmanager/TasksActivity.java
@@ -2,6 +2,13 @@ package com.example.raajesharunachalam.taskmanager;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.raajesharunachalam.taskmanager.responses.Task;
 
 public class TasksActivity extends AppCompatActivity {
 
@@ -9,5 +16,44 @@ public class TasksActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tasks);
+    }
+
+    public class TasksAdapter extends RecyclerView.Adapter<TasksAdapter.TasksViewHolder>{
+
+        Task[] tasks;
+        public TasksAdapter(Task[] tasks){
+            this.tasks = tasks;
+        }
+
+        @Override
+        public TasksViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            LayoutInflater inflater = LayoutInflater.from(TasksActivity.this);
+            View view = inflater.inflate(R.layout.task_item, parent, false);
+            TasksViewHolder viewHolder = new TasksViewHolder(view);
+            return viewHolder;
+        }
+
+        @Override
+        public void onBindViewHolder(TasksViewHolder holder, int position) {
+            Task task = tasks[position];
+            holder.taskDescription.setText(task.getTaskDescription());
+            String name = task.getFirstName() + " " + task.getLastName();
+            holder.taskAuthor.setText(name);
+        }
+
+        @Override
+        public int getItemCount() {
+            return tasks.length;
+        }
+
+        public class TasksViewHolder extends RecyclerView.ViewHolder{
+            TextView taskDescription;
+            TextView taskAuthor;
+            public TasksViewHolder(View itemView) {
+                super(itemView);
+                taskDescription = (TextView) findViewById(R.id.task_description);
+                taskAuthor = (TextView) findViewById(R.id.task_author);
+            }
+        }
     }
 }
--- app/src/main/java/com/example/raajesharunachalam/taskmanager/responses/Task.java
+++ app/src/main/java/com/example/raajesharunachalam/taskmanager/responses/Task.java
@@ -12,4 +12,19 @@ public class Task {
 
     @SerializedName("last_name")
     private String lastName;
+
+    @SerializedName("task_description")
+    private String taskDescription;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getTaskDescription() {
+        return taskDescription;
+    }
 }
