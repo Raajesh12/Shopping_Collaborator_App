ea5fb540e86068c83e898c8b47d8d6b5e58b463a
vishnujaganathan <30843744+vishnujaganathan@users.noreply.github.com>
2017-08-14 17:43:47 -0500

    tasksactivity updated

--- app/src/main/java/com/example/raajesharunachalam/taskmanager/TasksActivity.java
+++ app/src/main/java/com/example/raajesharunachalam/taskmanager/TasksActivity.java
@@ -1,21 +1,72 @@
 package com.example.raajesharunachalam.taskmanager;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.raajesharunachalam.taskmanager.endpoints.GroupEndpoints;
+import com.example.raajesharunachalam.taskmanager.endpoints.TaskEndpoints;
+import com.example.raajesharunachalam.taskmanager.responses.Group;
+import com.example.raajesharunachalam.taskmanager.responses.GroupListResponse;
 import com.example.raajesharunachalam.taskmanager.responses.Task;
+import com.example.raajesharunachalam.taskmanager.responses.TaskListResponse;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class TasksActivity extends AppCompatActivity {
+
+    public static long gid;
+    RecyclerView rv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tasks);
+        Intent intent = getIntent();
+        gid = intent.getLongExtra(IntentKeys.GID, 0L);
+
+        rv = (RecyclerView) findViewById(R.id.recycle_tasks);
+
+        initializeRecyclerView(gid);
+
+    }
+
+    public void initializeRecyclerView(final long gid) {
+        Call<TaskListResponse> call = TaskEndpoints.taskEndpoints.getTasks(gid);
+
+        call.enqueue(new Callback<TaskListResponse>() {
+            @Override
+            public void onResponse(Call<TaskListResponse> call, Response<TaskListResponse> response) {
+                if (response.code() == ResponseCodes.HTTP_OK) {
+                    LinearLayoutManager layoutManager = new LinearLayoutManager(TasksActivity.this);
+                    rv.setLayoutManager(layoutManager);
+
+                    Task[] tasks = response.body().getTasks();
+                    TasksActivity.TasksAdapter tasksAdapter = new TasksActivity.TasksAdapter(tasks);
+                    rv.setAdapter(tasksAdapter);
+
+                    Log.d("ResponseInfo", "Code: " + String.valueOf(response.code()));
+                    Log.d("ResponseInfo", "Task Length: " + String.valueOf(tasks.length));
+                } else {
+                    Toast.makeText(TasksActivity.this, R.string.server_error, Toast.LENGTH_LONG).show();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<TaskListResponse> call, Throwable t) {
+                Toast.makeText(TasksActivity.this, R.string.call_failed, Toast.LENGTH_LONG).show();
+            }
+        });
     }
 
     public class TasksAdapter extends RecyclerView.Adapter<TasksAdapter.TasksViewHolder>{
