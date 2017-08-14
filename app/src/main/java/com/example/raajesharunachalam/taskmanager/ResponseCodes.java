0212d075660b5cded3a794f1e7d8f786c0836f27
Raajesh12 <raajstar3@gmail.com>
2017-08-14 15:26:50 -0500

    ResponseCodes class

---
+++ app/src/main/java/com/example/raajesharunachalam/taskmanager/ResponseCodes.java
@@ -0,0 +1,16 @@
+package com.example.raajesharunachalam.taskmanager;
+
+/**
+ * Created by raajesharunachalam on 8/14/17.
+ */
+
+public class ResponseCodes {
+    public static final int HTTP_OK = 200;
+    public static final int HTTP_CREATED = 201;
+    public static final int HTTP_NO_CONTENT = 204;
+    public static final int HTTP_BAD_REQUEST = 400;
+    public static final int HTTP_UNAUTHORIZED = 401;
+    public static final int HTTP_FORBIDDEN = 403;
+    public static final int HTTP_NOT_FOUND = 404;
+    public static final int HTTP_SERVER_ERROR = 500;
+}
