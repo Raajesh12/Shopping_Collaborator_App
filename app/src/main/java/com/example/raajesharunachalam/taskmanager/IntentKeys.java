d5da65d58618ac955a9caa01cbccc737eb7be192
vishnujaganathan <30843744+vishnujaganathan@users.noreply.github.com>
2017-08-14 15:49:47 -0500

    fixed signuppage and added intentkeys

---
+++ app/src/main/java/com/example/raajesharunachalam/taskmanager/IntentKeys.java
@@ -0,0 +1,10 @@
+package com.example.raajesharunachalam.taskmanager;
+
+/**
+ * Created by vishn on 8/14/2017.
+ */
+
+public class IntentKeys {
+    public static final String UID = "uid";
+    public static final String GID = "gid";
+}
--- app/src/main/java/com/example/raajesharunachalam/taskmanager/SignUpPage.java
+++ app/src/main/java/com/example/raajesharunachalam/taskmanager/SignUpPage.java
@@ -1,12 +1,23 @@
 package com.example.raajesharunachalam.taskmanager;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.raajesharunachalam.taskmanager.endpoints.UserEndpoints;
+import com.example.raajesharunachalam.taskmanager.requests.CreateUserRequest;
+import com.example.raajesharunachalam.taskmanager.responses.UIDResponse;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class SignUpPage extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,8 +32,7 @@ public class SignUpPage extends AppCompatActivity {
         });
     }
 
-    public void submitinfo()
-    {
+    public void submitinfo() {
         EditText first_name_edit = (EditText) findViewById(R.id.first_name);
         String first_name = first_name_edit.getText().toString();
 
@@ -34,6 +44,35 @@ public class SignUpPage extends AppCompatActivity {
 
         EditText passwordfield = (EditText) findViewById(R.id.passwordfield);
         String password = passwordfield.getText().toString();
+
+        CreateUserRequest request = new CreateUserRequest(first_name, last_name, email, password);
+        Call<UIDResponse> call = UserEndpoints.userEndpoints.createUser(request);
+        call.enqueue(new Callback<UIDResponse>() {
+            @Override
+            public void onResponse(Call<UIDResponse> call, Response<UIDResponse> response) {
+                if (response.code() == ResponseCodes.HTTP_BAD_REQUEST) {
+                    Toast.makeText(SignUpPage.this, "Email already used!", Toast.LENGTH_LONG).show();
+                } else if (response.code() == ResponseCodes.HTTP_SERVER_ERROR) {
+                    Toast.makeText(SignUpPage.this, "Server error.", Toast.LENGTH_LONG).show();
+                } else if (response.code() == ResponseCodes.HTTP_CREATED) {
+                    UIDResponse uidObject = response.body();
+                    int uid = (int) uidObject.getUid();
+                    Intent intent = new Intent(SignUpPage.this, GroupsActivity.class);
+                    intent.putExtra("uid", IntentKeys.UID);
+                    startActivity(intent);
+                }
+
+
+            }
+
+            @Override
+            public void onFailure(Call<UIDResponse> call, Throwable t) {
+
+                Toast.makeText(SignUpPage.this, "Network Error, try again.", Toast.LENGTH_LONG).show();
+            }
+        });
+
+
     }
 
 }
