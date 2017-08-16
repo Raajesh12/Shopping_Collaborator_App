48e16b4e066492f81ddb406238a25395f139387b
vishnujaganathan <30843744+vishnujaganathan@users.noreply.github.com>
2017-08-16 15:19:11 -0500

    created usersingroupactivity

---
+++ app/src/main/java/com/example/raajesharunachalam/taskmanager/UsersInGroupActivity.java
@@ -0,0 +1,13 @@
+package com.example.raajesharunachalam.taskmanager;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class UsersInGroupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_users_in_group);
+    }
+}
